fd26a988d09ee9dea2d76024cd9ce1116721ec25
P Sambit Kumar <64432227+thepsambit@users.noreply.github.com>
2020-07-25 10:57:52 +0530

    Update windows_startup_script.py

--- windows_startup_script.py
+++ windows_startup_script.py
@@ -3,6 +3,6 @@ API_KEY="Type that here"
 API_HASH="Type that here"
 #get it from my.telegram.org
 bot = TelegramClient('userbot',API_KEY,API_HASH)
-bot.start()
+bot.alive()
 
 #This script wont run your bot, it just generates a session.
